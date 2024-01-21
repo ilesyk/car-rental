4cc78c67f79c74127f1da25a0cc4f5a4d7d18744
ilesyk <pavloyavorskyi@ukr.net>
2024-01-21 18:32:17 +0200

    corections

--- src/components/CatalogPage/CarsList/CarsList.jsx
+++ src/components/CatalogPage/CarsList/CarsList.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectCars, selectFavorites, selectFilteredCars } from "../../../redux/selectors";
 import { CarsListWrapper } from "./CarsList.styled"
 import { ListItem } from "./ListItem"
+import { Link } from 'react-router-dom';
 
 export const CarsList = ({ isFavorite, currentPage, setCurrentPage }) => {
   const cars = useSelector(selectCars);
@@ -15,35 +16,57 @@ export const CarsList = ({ isFavorite, currentPage, setCurrentPage }) => {
   return (
     <>
       {isFavorite && !favoriteCars.length ? (
-        <h2 style={{ maxWidth: "320px", margin: "0 auto" }}>
-          Your favorites list is empty
-        </h2>
+        <>
+          <h2 style={{ maxWidth: '320px', margin: '50px auto 50px' }}>
+            Your favorites list is empty
+          </h2>
+          <Link to="/catalog">
+            <button
+              style={{
+                margin: '0 auto',
+                display: 'flex',
+                height: '48px',
+                padding: '14px 44px',
+                justifyContent: 'center',
+                border: 'none',
+                alignItems: 'center',
+                borderRadius: '12px',
+                background: '#3470FF',
+                color: 'white',
+              }}
+            >
+              Go to catalog
+            </button>
+          </Link>
+        </>
       ) : (
         <CarsListWrapper>
           <ListItem
             cars={
               isFavorite
                 ? favoriteCars
-                :  (!filteredCars.length?cars:filteredCars)
+                : !filteredCars.length
+                ? cars
+                : filteredCars
             }
           />
         </CarsListWrapper>
       )}
       {currentPage * 12 <=
-       (filteredCars.length ? filteredCars.length : cars.length) &&
+        (filteredCars.length ? filteredCars.length : cars.length) &&
         !isFavorite && (
           <button
             style={{
-              color: "#3470FF",
-              fontSize: "16px",
-              fontWeight: "500",
-              lineHeight: "1.5",
-              textDecorationLine: "underline",
-              border: "none",
-              background: "transparent",
-              margin: "0 auto 150px",
-              cursor: "pointer",
-              display: "block",
+              color: '#3470FF',
+              fontSize: '16px',
+              fontWeight: '500',
+              lineHeight: '1.5',
+              textDecorationLine: 'underline',
+              border: 'none',
+              background: 'transparent',
+              margin: '0 auto 150px',
+              cursor: 'pointer',
+              display: 'block',
             }}
             onClick={loadMoreCars}
           >
