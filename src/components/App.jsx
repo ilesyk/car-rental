8dbcc58ac636f09b48dc9eb433e25f08a3e2a75b
Pavlo Yavorskyi <124560323+ilesyk@users.noreply.github.com>
2024-01-21 20:04:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { lazy } from "react";
 import { Toaster } from "react-hot-toast";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/HomePage";
 import { GlobalStyle } from "./GlobalStyle";
 import { SharedLayout } from "./SharedLayout/SharedLayout";
@@ -15,7 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/catalog" element={<Catalog isFavorite={false} />} />
           <Route path="/favorite" element={<Catalog isFavorite={true} />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
       <GlobalStyle />
